aeeba2e08afd96349a59858d5046f5aa530a84d2
sa-es-ir <s.esmaeelinejad@gmail.com>
2023-05-22 00:01:35 +0700

    add middleware B

---
+++ MiddlewareTransferData/Middlewares/MiddlewareB.cs
@@ -0,0 +1,29 @@
+﻿namespace MiddlewareTransferData.Middlewares;
+
+public class MiddlewareB
+{
+
+    private readonly RequestDelegate _next;
+
+    public MiddlewareB(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context, TransferDataService transferService)
+    {
+        // Approach 1: using request headers
+        var aData1 = context.Request.Headers["A-Data"];
+
+        // Approach 2: using HttpContext Items
+        var aData2 = context.Items["A-Data"];
+
+        // Aproach 3: using HttpContext features
+        var aData3 = context.Features.Get<TransferDataFeature>();
+
+        // Aproach 4: using Scoped services
+        var aData4 = transferService.AData;
+
+        await _next(context);
+    }
+}
